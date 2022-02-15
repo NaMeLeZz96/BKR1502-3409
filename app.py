55ca4f7daf2e91b1760302d1f97c9090b9e5ba87
NaMeLeZz96 <99230802+NaMeLeZz96@users.noreply.github.com>
2022-02-15 12:12:05 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -19,26 +19,26 @@ app = Flask(__name__)
 from flask import request, render_template
 from keras.models import load_model
 
-@app.route("/", methods = ["GET", "POST"])
+@app.route("/", methods=["GET","POST"])
 def index():
-    if request.method == "POST":
+    if request.method=="POST":
         NPTA = request.form.get("NPTA")
         TLTA = request.form.get("TLTA")
         WCTA = request.form.get("WCTA")
-        print(NPTA,TLTA,WCTA)
+        print(NPTA, TLTA, WCTA)
         model = load_model("BKRNN")
-        pred = model.predict([[float(NPTA), float(TLTA), float(WCTA)]])
+        pred = model.predict([[float(NPTA),float(TLTA),float(WCTA)]])
         print(pred)
-        s = "The predicted bankruptcy score is: " + str(pred)
-        return(render_template("index.html", result = s))
+        s = "The predicted bankruptcy score is:" + str(pred)
+        return(render_template("index.html",result=s))
     else:
-        return(render_template("index.html", result = "2"))
+        return(render_template("index.html",result="2"))
 
 
 # In[ ]:
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
 
 
